857839c3ee4c63a423e6017193c6da81b69d085e
Ze'ev Goshen <zeevgoshen@gmail.com>
2021-10-18 13:03:13 +0300

    Add files via upload

---
+++ MyShop/Models/BasketItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyShop.Models
+{
+    public class BasketItem : BaseEntity
+    {
+        public string BasketId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
